da0586c8d474facd9069d06ca6611f219f3b05d0
kyle.heller <kyle.heller@gmail.com>
2022-11-21 16:52:46 -0500

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+// debugger;
+   let sum = 0
+    if ((a < 0) || (b < 0)) { 
+        let iserror = "ERROR"
+        return iserror
+    }
+    else if ((typeof a !== 'number') || (typeof b !== 'number')) {
+        let iserror = "ERROR"
+        return iserror
+    }
+    else if (Number(a) < Number(b)) {
+        for (let i = Number(a); i <= Number(b); i++) {
+            sum = sum + i
+        }
+    
+    }
+    else if (Number(a) > Number(b)) {
+        for (let i = Number(b); i <= Number(a); i++) {
+            sum = sum + i
+        }
+    }
+    return sum;
 };
+
+// console.log(sumAll(1, "90"))
 
 // Do not edit below this line
 module.exports = sumAll;
