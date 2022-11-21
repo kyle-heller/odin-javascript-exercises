f580433c5b4a03308e1b9323eb64c8e1b7ccf857
kyle.heller <kyle.heller@gmail.com>
2022-11-21 15:27:04 -0500

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+let removeFromArray = function(input, ...args) {
+    
+    for (let i = 0; i < args.length; i++) {
 
-};
+    let index = input.indexOf(args[i]);
+    if (index >= 0) {
+     parsed = input.splice(index,1)
+
+    }
+    }
+    return input
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
