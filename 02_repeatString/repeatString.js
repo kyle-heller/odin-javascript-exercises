18993814d52a4303a9ac02f8d2ef94a8640ba64f
kyle.heller <kyle.heller@gmail.com>
2022-11-21 14:24:00 -0500

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i = 0
+    message = ""
+    if (num < 0) {
+        return "ERROR"
+    }
+    while (i < Number(num)) {
+        message += string
+        i++
+    }
+    return message
 };
 
 // Do not edit below this line
