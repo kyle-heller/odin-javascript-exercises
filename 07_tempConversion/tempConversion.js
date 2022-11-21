9aafc216acae5da081738e7eaa57324fd09b3882
kyle.heller <kyle.heller@gmail.com>
2022-11-21 17:15:26 -0500

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,19 @@
-const convertToCelsius = function() {
+//subtract 32 and multiply by .5556 
+
+const convertToCelsius = function(tempF) {
+  temperature = ((tempF - 32) * .5556)
+  temperature = Math.round(temperature * 10) / 10
+  return temperature
 };
 
-const convertToFahrenheit = function() {
+//multiply by 1.8 and add 32.
+const convertToFahrenheit = function(tempC) {
+  temperature = (tempC * 1.8 + 32)
+  temperature = Math.round(temperature * 10) / 10
+  return temperature
 };
 
-// Do not edit below this line
+
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
