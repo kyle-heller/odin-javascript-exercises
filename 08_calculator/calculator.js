58d08b298f02d10c1e0f0eb61003966c038b88c6
kyle.heller <kyle.heller@gmail.com>
2022-11-25 18:51:58 -0500

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(num1=0, num2=0) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(num1, num2) {
+	return num1 - num2;
 
 };
 
-const power = function() {
+const sum = function(array) {
+  let output = 0;
+    for (let i = 0; i < array.length; i++) {
+      output += Number(array[i]);
+    }
+    return output;
+  }
+  // return array.reduce((total, current) => total + current, 0);
+
+// };
+
+const multiply = function(array) {
+  let output = 1; 
+ 
+  // Loop over the array starting from index 0 to it's length. 
+  for (let i = 0; i < array.length; ++i) { 
+    output *= Number(array[i]); 
+  } 
+  return output;
+};
+
+const power = function(num, power) {
+
+  let output = 1; 
+ 
+  for (let i = 0; i < power; ++i) { 
+    output *= num
+  } 
+  return output;
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
 	
+  let output = 1;
+
+  for (let i = 1; i <= num; i++) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
