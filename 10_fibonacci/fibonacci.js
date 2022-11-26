2087159006267fbdf2b08ee103f2e36ffd55d7b9
kyle.heller <kyle.heller@gmail.com>
2022-11-25 20:41:28 -0500

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    console.log(num)
+    let fibArray = [1, 1]
+    let array = function() {
+        for (let i = 1; i < num; i++) {
+            fibArray.push(fibArray.slice(-1)[0] + fibArray.slice(-2)[0])
+        }
+    }
+    array()
+    console.log(fibArray)
+    console.log(fibArray[num])
 
+    if (num <= 0) {
+        return "OOPS"
+    }
+    else {
+    return fibArray.slice(-2)[0];
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// ```javascript
+// fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
+// fibonacci(6) // returns 8
+// ```
