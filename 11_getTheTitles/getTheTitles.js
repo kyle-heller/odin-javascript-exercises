b8670228673c5a319a1c9e57cff1099538bf847f
kyle.heller <kyle.heller@gmail.com>
2022-11-25 21:16:56 -0500

    Completed 11_get_the_titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+  
+const getTheTitles = function(bookArray) {
+    let titleArray = []
+    for (let i = 0; i < bookArray.length; i++) {
+        let itemArray = bookArray[i]
+        let test = itemArray.title;
+        titleArray.push(test)
+    }
+    console.log(titleArray)
+    return titleArray;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
